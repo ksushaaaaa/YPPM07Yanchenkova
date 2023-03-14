b30fdd3400598f6cdc9d8abf83859e5edd79b331
ksushaaaaa <94594478+ksushaaaaa@users.noreply.github.com>
2023-03-14 19:21:51 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Kursach.Classes;
+using Kursach.Pages;
+
+namespace Kursach
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Manager.MainFrame = MainWindowFrame;
+            Manager.MainFrame.Navigate(new AuthPage());
+        }
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+        private void MainWindowFrame_ContentRendered(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainWindowFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+    }
+
+    
